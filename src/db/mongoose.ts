ad5b321488340695a44cdbb76be8322715445377
Ajay <ajaykumawat9904@gmail.com>
2025-08-20 14:27:19 +0530

    - This commit represents a complete removal of the existing codebase.

--- src/db/mongoose.ts
+++
@@ -1,9 +0,0 @@
-import mongoose from 'mongoose';
-import { env } from '../config/env';
-import { logger } from '../utils/logger';
-
-export async function connectDB() {
-  mongoose.set('strictQuery', true);
-  await mongoose.connect(env.MONGO_URI);
-  logger.info('MongoDB connected');
-}
--- src/middleware/rateLimit.ts
+++
@@ -1,8 +0,0 @@
-import rateLimit from "express-rate-limit";
-
-export const authLimiter = rateLimit({
-  windowMs: 10 * 60 * 1000, // 10 minutes
-  max: 100, // per IP
-  standardHeaders: true,
-  legacyHeaders: false,
-});
